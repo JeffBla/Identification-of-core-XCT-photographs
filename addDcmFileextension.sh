5fa3b76245513f4863796559eaa5f26d629ae313
JeffBla <jeff920305@gmail.com>
2023-08-31 11:14:27 +0800

    let all dcm file have the .dcm
    from now on all the dcm file should have .dcm so as to decrease incoming problems

--- addDcmFileextension.sh
+++ addDcmFileextension.sh
@@ -1,7 +1,10 @@
-cd '/home/jeffbla/resourceDPProject/bh-3 DICOM-20230421T075124Z-001/bh-3 DICOM/bh_3_18/13300001'
-mkdir ../13300001_dcm
+prefix='/home/jeffbla/resourceDPProject/bh-3 DICOM-20230421T075124Z-001/bh-3 DICOM/bh_3_'
 
-for file in $(ls '.')
-do
-    cp $file "/home/jeffbla/resourceDPProject/bh-3 DICOM-20230421T075124Z-001/bh-3 DICOM/bh_3_18/13300001_dcm/${file}.dcm"
+target='20/13420001'
+
+cd "${prefix}${target}"
+mkdir ../13420001_dcm
+
+for file in $(ls '.'); do
+    cp $file "${prefix}${target}_dcm/${file}.dcm"
 done
